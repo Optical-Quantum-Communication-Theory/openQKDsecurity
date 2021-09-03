d9c1eb6705b4861b1f5bd4c201dc524a990490ea
Wenyuan <wywang.kyme@gmail.com>
2021-09-03 08:27:05 +0800

    Update perturbation_channel.m

--- Solvers/solver_helpers_common/perturbation_channel.m
+++ Solvers/solver_helpers_common/perturbation_channel.m
@@ -3,12 +3,13 @@ function [rhoPrime,epsilon] = perturbation_channel(rho)
     dim = size(rho,1);
     eigMin = lambda_min(rho);
     epsilon=0;
+    rho = (rho+rho')/2;
     rhoPrime = rho;
     if   eigMin<=0
-        if trace(rho)  > 0 
+        if real(trace(rho))  > 0 
        
 
-            epsilon = (eigMin  * dim)/(eigMin*dim - trace(rho)) + default_perturbation;
+            epsilon = (eigMin  * dim)/(eigMin*dim - real(trace(rho))) + default_perturbation;
                  % check again realEpsilon is in the range where the
                  %   theorem can apply
           	if epsilon < 0 || epsilon > 1/(exp(1)*(dim-1))
@@ -16,7 +17,8 @@ function [rhoPrime,epsilon] = perturbation_channel(rho)
                     fprintf("**** Error: Perturbation failed. The smallest eigenvalue is less than zero. ****\n")
                     throw(ME);
            	else
-                    rhoPrime = (1-epsilon) * rho + epsilon * trace(rho)*eye(dim)/dim;
+                    rhoPrime = (1-epsilon) * rho + epsilon * real(trace(rho))*eye(dim)/dim;
+                    rhoPrime = (rhoPrime + rhoPrime')/2;
            	end % end of if for validity of realEpsilon
         else
              ME = MException('perturbation_channel:badRho','Trace of rho should be positive');
@@ -24,15 +26,15 @@ function [rhoPrime,epsilon] = perturbation_channel(rho)
        	end % end of check perturbed rho
         
    
-        eigMin2 = lambda_min(rhoPrime);
-           
-        % check again the perturbed rho.
-                
-        if eigMin2<=0
-%             ME = MException('perturbation_channel:badRhobadEpsilon','Please check your rho and epsilon');
-            fprintf('**** Error: lambda_min still negative, at %f ****\n',eigMin2);
-%             throw(ME);
-        end
+%         eigMin2 = lambda_min(rhoPrime);
+%            
+%         % check again the perturbed rho.
+%                 
+%         if eigMin2<=-eps
+% %             ME = MException('perturbation_channel:badRhobadEpsilon','Please check your rho and epsilon');
+%             fprintf('**** Error: lambda_min still negative, at %f ****\n',eigMin2);
+% %             throw(ME);
+%         end
   	end     
 
 end
